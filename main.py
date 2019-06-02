b175bc539eff1aff4f631ca4a18af9650ebc2dd8
joao <jovitor737@gmail.com>
2019-06-02 02:06:38 -0300

    add INI

--- main.py
+++ main.py
@@ -1,62 +1,48 @@
+from configparser import ConfigParser
 from itertools import count
-from json import dump, load, decoder
+from json import load
 from random import choice
-
-"""
-\033[31m  =  Red
-\033[32m  =  Green
-\033[34m  =  Blue
-\033[m    =  Remove
-"""
 
 
 class PyShell:
     def __init__(self):
-        with open('files/names.json', 'r') as names:
-            print(choice(load(names)))
+        with open('files/banner.json') as banner:
+            print(choice(load(banner)))
 
-        self.user_globals = self.load_user_globals()
+        self.user_globals = ConfigParser()
+        self.user_globals.read('files/user_globals.ini')
+        if not self.user_globals.has_section('USER_GLOBALS'):
+            self.user_globals.add_section('USER_GLOBALS')
+
         self.user_input()
 
     def user_input(self):
         for i in count():
             try:
                 self.exec_user_input(i)
-
             except EOFError:
                 print()
                 break
-
             except Exception as e:
                 print(f'\033[31m{e.__class__.__name__}: {e}\033[m')
 
     def exec_user_input(self, i):
         user_input = input(f'\033[32m\nIn [{i}]: \033[m')
+        globals = dict(self.user_globals['USER_GLOBALS'])
+
         try:
-            retval = eval(user_input, self.user_globals)
+            retval = eval(user_input, globals)
             if retval:
                 print(f'\033[34mOut [{i}]:\033[m {retval}')
-
         except SyntaxError:
-            exec(user_input, self.user_globals)
+            exec(user_input, globals)
             if '=' in user_input:
-                self.save_user_globals()
+                for key, value in globals.items():
+                    if key[0] != '_' and isinstance(value, (list, dict, str, int, float, bool)):
+                        self.user_globals.set('USER_GLOBALS', key, str(value))
 
-    def save_user_globals(self):
-        user_globals = self.user_globals.copy()
-        user_globals.pop('__builtins__')
-        user_globals = dict(
-            item for item in user_globals.items() if isinstance(item[1], (list, dict, str, int, float, bool)))
-        with open('files/user_globals.json', 'w') as file:
-            dump(user_globals, file, indent=2, sort_keys=True)
-
-    @staticmethod
-    def load_user_globals():
-        try:
-            with open('files/user_globals.json', 'r') as file:
-                return load(file)
-        except decoder.JSONDecodeError:
-            return {}
+                with open('files/user_globals.ini', 'w') as f:
+                    self.user_globals.write(f)
 
 
 if __name__ == '__main__':
