1535c69bca4a7bf2534eb9141bbee8888e1cad68
joao <jovitor737@gmail.com>
2019-06-14 17:59:52 -0300

    add completer

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import readline
 from configparser import ConfigParser
 from itertools import count
 from json import load
@@ -9,40 +12,57 @@ class PyShell:
         with open('files/banner.json') as banner:
             print(choice(load(banner)))
 
-        self.user_globals = ConfigParser()
-        self.user_globals.read('files/user_globals.ini')
-        if not self.user_globals.has_section('USER_GLOBALS'):
-            self.user_globals.add_section('USER_GLOBALS')
+        self._user_globals = ConfigParser()
+        self._user_globals.read('files/user_globals.ini')
+        if not self._user_globals.has_section('USER_GLOBALS'):
+            self._user_globals.add_section('USER_GLOBALS')
 
-        self.user_input()
+        readline.parse_and_bind("tab: complete")
+        readline.set_completer(self.completer)
 
-    def user_input(self):
+        self.main_loop()
+
+    @property
+    def user_globals(self):
+        return dict(self._user_globals['USER_GLOBALS'])
+
+    @user_globals.setter
+    def user_globals(self, l):
+        pass
+
+    @staticmethod
+    def completer(text, state):
+        options = [i for i in dir(__builtins__) if i.startswith(text)]
+        if state < len(options):
+            return options[state]
+        else:
+            return None
+
+    def main_loop(self):
         for i in count():
             try:
-                self.exec_user_input(i)
-            except EOFError:
+                user_input = input('\033[32m\nIn [{}]: \033[m'.format(i))
+                self.exec_user_input(user_input)
+            except (EOFError, KeyboardInterrupt):
                 print()
                 break
             except Exception as e:
-                print(f'\033[31m{e.__class__.__name__}: {e}\033[m')
+                print('\033[33m{}: {}\033[m'.format(e.__class__.__name__, e))
 
-    def exec_user_input(self, i):
-        user_input = input(f'\033[32m\nIn [{i}]: \033[m')
-        globals = dict(self.user_globals['USER_GLOBALS'])
-
+    def exec_user_input(self, user_input):
         try:
-            retval = eval(user_input, globals)
+            retval = eval(user_input, self.user_globals)
             if retval:
-                print(f'\033[34mOut [{i}]:\033[m {retval}')
+                print('\033[34mOut [{}]:\033[m {}'.format(i, retval))
         except SyntaxError:
-            exec(user_input, globals)
+            exec(user_input, self.user_globals)
             if '=' in user_input:
-                for key, value in globals.items():
+                for key, value in self.user_globals.items():
                     if key[0] != '_' and isinstance(value, (list, dict, str, int, float, bool)):
-                        self.user_globals.set('USER_GLOBALS', key, str(value))
+                        self._user_globals.set('USER_GLOBALS', key, str(value))
 
                 with open('files/user_globals.ini', 'w') as f:
-                    self.user_globals.write(f)
+                    self._user_globals.write(f)
 
 
 if __name__ == '__main__':
